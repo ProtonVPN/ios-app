96f6df645ca04cd7776b361a708627b5a65e06a5
Andrian Sergheev <sergheev@icloud.com>
2020-01-31 08:50:31 +0100

    Update UIImage+Extension.swift
    removed force unwrap & update to the new graphics api

--- ProtonVPN/Extentions/UIImage+Extension.swift
+++ ProtonVPN/Extentions/UIImage+Extension.swift
@@ -24,32 +24,32 @@ import Foundation
 import UIKit
 
 extension UIImage {
-    
-    /// Returns a image that fills in newSize
-    func resizedImage(newSize: CGSize) -> UIImage {
-        // Guard newSize is different
-        guard self.size != newSize else { return self }
-        
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
-        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
-    /// Returns a resized image that fits in rectSize, keeping it's aspect ratio
-    /// Note that the new image size is not rectSize, but within it.
-    func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
-        let widthFactor = size.width / rectSize.width
-        let heightFactor = size.height / rectSize.height
-        
-        var resizeFactor = widthFactor
-        if size.height > size.width {
-            resizeFactor = heightFactor
-        }
-        
-        let newSize = CGSize(width: size.width / resizeFactor, height: size.height / resizeFactor)
-        let resized = resizedImage(newSize: newSize)
-        return resized
-    }
+
+	/// Returns a image that fills in newSize
+	func resizedImage(newSize: CGSize) -> UIImage {
+		// Guard newSize is different
+		guard self.size != newSize else { return self }
+
+		let renderer = UIGraphicsImageRenderer(size: self.size)
+		return renderer.image { context in
+			self.draw(in: CGRect(origin: CGPoint.zero, size: self.size))
+		}
+	}
+
+	/// Returns a resized image that fits in rectSize, keeping it's aspect ratio
+	/// Note that the new image size is not rectSize, but within it.
+	func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
+		let widthFactor = size.width / rectSize.width
+		let heightFactor = size.height / rectSize.height
+
+		var resizeFactor = widthFactor
+		if size.height > size.width {
+			resizeFactor = heightFactor
+		}
+
+		let newSize = CGSize(width: size.width / resizeFactor, height: size.height / resizeFactor)
+		let resized = resizedImage(newSize: newSize)
+		return resized
+	}
 }
+
